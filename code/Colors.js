eca93c8608f01e3e9723cc2cd3677bccc03780d1
Tobias Hassel <tobias.hassel1@web.de>
2021-05-02 23:01:48 +0200

    Refs #40 - Add simple layout for CreateAccountScreen

--- code/Colors.js
+++ code/Colors.js
@@ -6,5 +6,7 @@ export default class Colors {
   static get UI_Light_2() { return "#F28D35"; }
   static get UI_Light_3() { return "#732C02"; }
   static get UI_Light_4() { return "#F2F2F2"; }
+  static get Facebook_Blue() { return "#4267B2"; }
+  static get Google_Grey() { return "#EBEAEC"; }
 }
 
--- code/screens/CreateAccountScreen.js
+++ code/screens/CreateAccountScreen.js
@@ -1,19 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
+import { Pressable, CheckBox, StyleSheet, Text, View, TextInput } from 'react-native';
 import Colors from '../Colors';
 
 
 export default class CreateAccountScreen extends Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <Button
-          title="test"
-          color={Colors.UI_Light_2}
-          onPress={() => this.props.navigation.navigate('Home')} />
+        <Text style={styles.h1}>Profil erstellen</Text>
+        <Pressable style={[styles.button, { backgroundColor: Colors.Facebook_Blue }]}>
+          <Text style={styles.buttonText}>MIT FACEBOOK ANMELDEN</Text>
+        </Pressable>
+        <Pressable style={[styles.button, { backgroundColor: '#fff', borderColor: Colors.Google_Grey }]}>
+          <Text style={[styles.buttonText, { color: "#000" }]}>MIT GOOGLE ANMELDEN</Text>
+        </Pressable>
+        <Text>ODER MIT EMAIL ANMELDEN</Text>
+        <TextInput
+          defaultValue='Name'
+          style={styles.inputField}
+        />
+        <TextInput
+          defaultValue='Email'
+          style={styles.inputField}
+        />
+        <TextInput
+          secureTextEntry={true}
+          style={styles.inputField}
+        />
+        <View style={styles.checkBoxContainer}>
+          <Text style={styles.checkboxText} onPress={() => Alert.alert("Lorem ipsum")}>Datenschutzrichtlinien gelesen</Text>
+          <CheckBox style={styles.checkbox} />
+        </View>
+        <Pressable style={styles.button} onPress={() => Alert.alert("Test")}>
+          <Text style={styles.buttonText}>Los geht's</Text>
+        </Pressable>
       </View>
     );
   };
@@ -28,6 +50,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  checkBoxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3,
+    width: 328,
+    height: 55,
+    borderRadius: 38,
+    margin: 9,
+    backgroundColor: Colors.UI_Light_2
+  },
+  inputField: {
+    width: 328,
+    height: 55,
+    color: '#333333',
+    backgroundColor: Colors.UI_Light_4,
+    borderRadius: 15,
+    margin: 9,
+    paddingLeft: 12,
+  },
+  buttonText: {
+    fontSize: 14,
+    color: Colors.UI_Light_4
+  },
+  h1: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    margin: 30
+  },
+  checkbox: {
+    borderRadius: 2,
+    alignSelf: "center",
+  },
+  checkboxText: {
+    margin: 8,
+  }
+
+
 });
 
 
