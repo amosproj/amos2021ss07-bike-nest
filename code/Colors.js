085bc4ba9b9048d63754de8527fadf288c7c51db
Tobias Hassel <tobias.hassel1@web.de>
2021-05-02 09:17:21 +0200

    Refs #40 - Add Colors class for easy access to global UI Colors

---
+++ code/Colors.js
@@ -0,0 +1,10 @@
+
+export default class Colors {
+
+  static get UI_Light_0() { return "#D6F2C9"; }
+  static get UI_Light_1() { return "#B5D99C"; }
+  static get UI_Light_2() { return "#F28D35"; }
+  static get UI_Light_3() { return "#732C02"; }
+  static get UI_Light_4() { return "#F2F2F2"; }
+}
+
