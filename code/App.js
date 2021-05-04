e40cc5b149e51d75680655149e658b58e1aa77e7
grunerlisa <lisa.gruner@fau.de>
2021-05-04 19:59:51 +0200

    initial commit - setup

--- code/App.js
+++ code/App.js
@@ -4,12 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { CreateAccountScreen } from './screens/CreateAccountScreen';
 import PersonalInformationScreen from './screens/PersonalInformationScreen';
 import LoginScreen from './screens/LoginScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 
 export default function App() {
   return (
     <NavigationContainer>
       <AppNavigator.Navigator initialRouteName="CreateAccountScreen">
+      <AppNavigator.Screen name='Payment' component={PaymentScreen} />
         <AppNavigator.Screen name='Login' component={LoginScreen} />
         <AppNavigator.Screen name='CreateAccount' component={CreateAccountScreen} />
         <AppNavigator.Screen name='EditPersonalInformation' component={PersonalInformationScreen} />
