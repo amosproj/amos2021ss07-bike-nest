15bee45b685dbfd2753208c9c7ec2c50076a26b9
Tobias Hassel <tobias.hassel1@web.de>
2021-05-09 18:00:22 +0200

    Refs #64 - Use mainStyles in PersonalInfo screen, Add Scrollview to CreateAccount screen in order to avoid keyboard overlapping

--- code/screens/CreateAccountScreen.js
+++ code/screens/CreateAccountScreen.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { CreateAccountManually } from '../components/CreateAccountManually';
 import { CreateAccountVia3rdParty } from '../components/CreateAccountVia3rdParty';
 import { mainStyles } from "../styles/MainStyles";
 
-export function CreateAccountScreen({ navigation }) {
+export function CreateAccountScreen() {
   return (
     <View style={mainStyles.container}>
-      <Text style={mainStyles.h1}>Profil erstellen</Text>
-      <CreateAccountVia3rdParty />
-      <Text>ODER MIT EMAIL ANMELDEN</Text>
-      <CreateAccountManually />
+      <ScrollView >
+
+        <View style={[mainStyles.container, { marginTop: 40 }]}>
+          <Text style={mainStyles.h1}>Profil erstellen</Text>
+        </View>
+
+        <CreateAccountVia3rdParty />
+
+        <View style={mainStyles.container}>
+          <Text style={mainStyles.stdText}>ODER MIT EMAIL ANMELDEN</Text>
+        </View>
+
+        <CreateAccountManually />
+      </ScrollView>
     </View>
   );
 }
