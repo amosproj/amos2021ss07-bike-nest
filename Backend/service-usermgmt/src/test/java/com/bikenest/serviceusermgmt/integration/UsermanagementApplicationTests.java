20d00ec91b7d3824372ef9329fa4bbde30bb38ee
richmandlx <herbstpeter19@gmail.com>
2021-05-11 14:46:05 +0200

    Rename usermanagement service and differentiate between integration and unit tests.

--- Backend/service-usermgmt/src/test/java/com/bikenest/serviceusermgmt/integration/UsermanagementApplicationTests.java
+++ Backend/service-usermgmt/src/test/java/com/bikenest/serviceusermgmt/integration/UsermanagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bikenest.usermanagement;
+package com.bikenest.serviceusermgmt.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
