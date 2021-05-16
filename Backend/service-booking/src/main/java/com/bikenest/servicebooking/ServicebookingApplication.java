b729a73c612875503a78fc549050c5c1396a3a4f
richmandlx <herbstpeter19@gmail.com>
2021-05-16 14:11:55 +0200

    #81 - Modify gradle settings for service-booking, so that it compiles.

--- Backend/service-booking/src/main/java/com/bikenest/servicebooking/ServicebookingApplication.java
+++ Backend/service-booking/src/main/java/com/bikenest/servicebooking/ServicebookingApplication.java
@@ -2,6 +2,7 @@ package com.bikenest.servicebooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class ServicebookingApplication {
     @RequestMapping("/info")
     public String home() {
         return "Booking info 1.";
+    }
+
+    @PostMapping("/commontest")
+    public String test(){
+        return "working.";
     }
 
 
