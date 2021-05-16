e860ff5565bb8c19e37ee75b8aa6e553c4ee7066
richmandlx <herbstpeter19@gmail.com>
2021-05-16 12:50:24 +0200

    Call the JWT Validation of UserMgmt Service from the API Gateway using the FeignClient.

--- Backend/apigateway/src/main/java/com/bikenest/apigateway/ApigatewayApplication.java
+++ Backend/apigateway/src/main/java/com/bikenest/apigateway/ApigatewayApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients(basePackageClasses = com.bikenest.apigateway.UsermgmtClient.class)
 public class ApigatewayApplication {
 
     @Autowired
