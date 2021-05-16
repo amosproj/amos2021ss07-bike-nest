3c1cc81d7d63960fc3c8708d076aa0f7e2a938bb
richmandlx <herbstpeter19@gmail.com>
2021-05-16 12:48:23 +0200

    Add FeignClient to the API Gateway.

---
+++ Backend/apigateway/src/main/java/com/bikenest/apigateway/FeignClientConfig.java
@@ -0,0 +1,26 @@
+package com.bikenest.apigateway;
+
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfig {
+    private ObjectFactory<HttpMessageConverters> messageConverters = HttpMessageConverters::new;
+
+    @Bean
+    Encoder feignFormEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+
+    @Bean
+    Decoder feignFormDecoder() {
+        return new SpringDecoder(messageConverters);
+    }
+}
