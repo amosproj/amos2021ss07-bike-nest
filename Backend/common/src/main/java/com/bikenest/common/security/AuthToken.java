9337febfa9d01e3b5348ef0b804f366a882fab33
richmandlx <herbstpeter19@gmail.com>
2021-05-20 18:21:03 +0200

    Add Authorities to the Backend Authorization.

--- Backend/common/src/main/java/com/bikenest/common/security/AuthToken.java
+++ Backend/common/src/main/java/com/bikenest/common/security/AuthToken.java
@@ -3,21 +3,37 @@ package com.bikenest.common.security;
 import io.jsonwebtoken.Claims;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+/**
+ * The @JWTAuthenticationFilter will set the Authentication Object of the SecurityContext to an Object of this class,
+ * if a JWT was provided. That object can be retrieved inside Endpoints by using SecurityContextHolder.getContext().getAuthentication()
+ * You can then Access a @UserInformation Object by calling getPrincipal on AuthToken. A better alternative to this is however
+ * to annotate a UserInformation Paramter in an controller endpoint with @AuthenticationPrincipal. Spring will then autoinject
+ * the UserInformation Object.
+ */
 public class AuthToken implements Authentication {
     private UserInformation userInformation;
 
-    public AuthToken(Claims claims){
+    public AuthToken(Claims claims) {
         this.userInformation = new UserInformation(claims);
     }
 
-    //This can be used with @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-    //TODO: Implement this later...
+    /**
+     * This can be used with @PreAuthorize("hasRole('USER') or hasRole('ADMIN')" to secure Endpoints.
+     * The Role is retrieved from the provided JWT. It is possible to add even finer grained Privileges here, for
+     * example one could add a READ_PRIVILEGE that will allow to access read only Endpoints.
+     */
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + userInformation.getRole().toString().toUpperCase()));
+        return authorities;
     }
 
     @Override
