e29b51533f0374101ce962bc11a4bc0bd88e5751
richmandlx <herbstpeter19@gmail.com>
2021-05-22 16:41:23 +0200

    Refactor Usermgmt Service (move functionality to services, so that they can be mocked)

--- Backend/service-usermgmt/src/main/java/com/bikenest/serviceusermgmt/UserController.java
+++ Backend/service-usermgmt/src/main/java/com/bikenest/serviceusermgmt/UserController.java
@@ -1,12 +1,12 @@
 package com.bikenest.serviceusermgmt;
 
 import com.bikenest.common.interfaces.usermgmt.*;
-import com.bikenest.serviceusermgmt.helper.JWTHelper;
+import com.bikenest.serviceusermgmt.services.AccountService;
+import com.bikenest.serviceusermgmt.services.JWTService;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.http.ResponseEntity;
 
 import java.security.Key;
-
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bikenest.serviceusermgmt.models.User;
-import com.bikenest.serviceusermgmt.repository.UserRepository;
 
 /**
  * Provides Endpoints for Signing in and Signing up
@@ -30,43 +29,44 @@ public class UserController {
     private Key SECRET_KEY = Keys.hmacShaKeyFor("NdRgUkXp2s5v8yzB?D(G+KbPeShVmYq3".getBytes());
 
 	@Autowired
-	UserRepository userRepository;
+	AccountService accountService;
+	@Autowired
+	JWTService jwtService;
 
     @PostMapping(path="/validatejwt")
 	@PreAuthorize("hasRole('SERVICE')")
-    public ResponseEntity<Boolean> ValidateJWT(@RequestBody String JWT){
-    	return ResponseEntity.ok(JWTHelper.GetSingleton().ValidateJWT(JWT));
+    public ResponseEntity<Boolean> validateJWT(@RequestBody String JWT){
+    	return ResponseEntity.ok(jwtService.validateJWT(JWT));
     }
 
 	@PostMapping("/signin")
 	public ResponseEntity<SigninResponse> authenticateUser(@Valid @RequestBody SigninRequest signinRequest) {
-		Optional<User> user = userRepository.findByEmail(signinRequest.getEmail());
-		if(!user.isPresent()){
+		if (accountService.existsAccountWithEmail(signinRequest.getEmail()))
 			return ResponseEntity.badRequest().body(new SigninResponse(false, "Email not found!", null));
-		}
-		if(signinRequest.getPassword().equals(user.get().getPassword()))
+
+		Optional<User> loggedInUser = accountService.loginUser(signinRequest.getEmail(), signinRequest.getPassword());
+		if(loggedInUser.isPresent())
 		{
-			String jwt = JWTHelper.GetSingleton().BuildJwtFromUser(user.get());
+			String jwt = jwtService.buildJwtFromUser(loggedInUser.get());
 			return ResponseEntity.ok(new SigninResponse(true, null, jwt));
 		}
+
 		return ResponseEntity.badRequest().body(new SigninResponse(false, "Invalid password!", null));
 	}
 
 	@PostMapping("/signup")
 	public ResponseEntity<SignupResponse> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+		if (accountService.existsAccountWithEmail(signUpRequest.getEmail())) {
 			return ResponseEntity
 					.badRequest()
 					.body(new SignupResponse(false,"Email is already taken!", null));
 		}
 
 		// Create new user's account
-		User user = new User(signUpRequest.getName(), signUpRequest.getLastname(), signUpRequest.getEmail(),
+		Optional<User> user = accountService.createAccount(signUpRequest.getName(), signUpRequest.getLastname(), signUpRequest.getEmail(),
 								signUpRequest.getPassword());
 
-		userRepository.save(user);
-
-		String jwt = JWTHelper.GetSingleton().BuildJwtFromUser(user);
+		String jwt = jwtService.buildJwtFromUser(user.get());
 
 		return ResponseEntity.ok(new SignupResponse(true, null, jwt));
 	}
