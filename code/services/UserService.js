d621fee498d53206e36eb1b4466da822e53d0aeb
Peter Herbst <janupet@t-online.de>
2021-05-24 11:46:23 +0200

    Finish frontend UserService.

--- code/services/UserService.js
+++ code/services/UserService.js
@@ -1,4 +1,5 @@
 import global from "../components/GlobalVars";
+import JwtDecoder from "../components/JwtDecoder";
 
 export class UserService{
 
@@ -7,7 +8,7 @@ export class UserService{
      * If success is false, then error will contain the error message. Also this function will automatically set the global JWT
      * if it's successful.
      * Use it like this:
-     * userService.LoginUser(email, password)
+     * userService.loginUser(email, password)
      *      .then(response =>{
      *          if(response?.success){
      *              navigation.navigate("FindBikeNest");
@@ -17,7 +18,7 @@ export class UserService{
      *      });
      * @param {string} email Email address as string
      * @param {string} password Password as string
-     * @returns Promise with Dictionary with the fields success and error.
+     * @returns {Promise<any | {success: boolean, error: any}>} A promise with a dictionary that contains the fields success and error. If success is false, you can read the error field.
      */
     async loginUser(email, password) {
         let request = {
@@ -45,4 +46,81 @@ export class UserService{
                 return {"success": false, "error": error}
             });
     }
+
+    /**
+     * Tries to sign up with the backend using email, password, first name and last name. Returns a promise with a dictionary. You can check the success field of this dictionary and
+     * if success is false, show the error message to the user.
+     * Use it like this:
+     * userService.registerUser(email, password, firstName, lastName)
+     *      .then(response =>{
+     *          if(response?.success){
+     *              setModalInfo("Erfolgreich registriert.");
+     *          }else{
+     *              setModalInfo(response?.error);
+     *          }
+     *      });
+     * @param email {string}
+     * @param password {string}
+     * @param firstName {string}
+     * @param lastName {string}
+     * @returns {Promise<any | {success: boolean, error: any}>} A promise with a dictionary that contains the fields success and error. If success is false, you can read the error field.
+     */
+    async registerUser(email, password, firstName, lastName){
+        let request = {
+            "name": firstName,
+            "lastname": lastName,
+            "email": email,
+            "password": password
+        };
+
+        return fetch(global.globalIPAddress + "/usermanagement/signup", {
+            method: 'POST',
+            body: JSON.stringify(request),
+            headers: { Accept: 'application/json', 'Content-Type': 'application/json' }
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                if(json.success){
+                    global.saveAuthenticationToken(json.jwt);
+                    return {"success": true, "error": null};
+                }else{
+                    return {"success": false, "error": json.error};
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                return {"success": false, "error": error}
+            });
+    }
+
+    /**
+     * Returns if the user is currently logged in or not.
+     * @returns {boolean}
+     */
+    isLoggedIn(){
+        //TODO: Actually check if the jwt is not expired
+        let jwt = global.getAuthenticationToken();
+        return jwt != null;
+    }
+
+    //TODO: These functions are not tested yet
+    getFirstName(){
+        let jwt = global.getAuthenticationToken();
+        let payload = JwtDecoder.decode(jwt);
+        return payload.FirstName;
+    }
+
+    getLastName(){
+        let jwt = global.getAuthenticationToken();
+        let payload = JwtDecoder.decode(jwt);
+        return payload.LastName;
+    }
+
+    getEmail(){
+        let jwt = global.getAuthenticationToken();
+        let payload = JwtDecoder.decode(jwt);
+        return payload.sub;
+    }
+
 }
