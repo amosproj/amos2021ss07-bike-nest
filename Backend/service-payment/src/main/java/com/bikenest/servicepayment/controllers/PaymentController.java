883860338ead48c801f2541d20a02a698d3508a8
Peter Herbst <60853627+richmandlx@users.noreply.github.com>
2021-05-26 22:12:54 +0200

    Start work on payment service. Communication to braintree doesn't seem to work.

--- Backend/service-payment/src/main/java/com/bikenest/servicepayment/controllers/PaymentController.java
+++ Backend/service-payment/src/main/java/com/bikenest/servicepayment/controllers/PaymentController.java
@@ -1,7 +1,12 @@
 package com.bikenest.servicepayment.controllers;
 
+import com.bikenest.common.security.UserInformation;
+import com.bikenest.servicepayment.services.PaymentService;
 import com.braintreegateway.BraintreeGateway;
 import com.braintreegateway.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/payment")
 public class PaymentController {
 
+    @Autowired
+    PaymentService paymentService;
+
     @GetMapping("/info")
     public String info(){
         return "Payment Service works!";
+    }
+
+    @GetMapping("/getclienttoken")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public String getClientToken(@AuthenticationPrincipal UserInformation userInformation){
+        return paymentService.generateClientToken(userInformation.getUserId());
     }
 
     @GetMapping("/registerpayment")
--- Backend/service-payment/src/main/java/com/bikenest/servicepayment/services/PaymentService.java
+++ Backend/service-payment/src/main/java/com/bikenest/servicepayment/services/PaymentService.java
@@ -1,16 +1,35 @@
 package com.bikenest.servicepayment.services;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.braintreegateway.BraintreeGateway;
+import com.braintreegateway.ClientTokenRequest;
+import com.braintreegateway.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
-    @Value("${BT_MERCHANT_ID}")
     private String merchantId;
+    private String publicKey;
+    private String privateKey;
+
+    private BraintreeGateway gateway;
 
     public PaymentService(){
-        if (merchantId == null){
-            throw new IllegalArgumentException("BT_MERCHANT_ID Environment Variable is not set.");
+        merchantId = System.getenv("BT_MERCHANT_ID");
+        publicKey = System.getenv("BT_PUBLIC_KEY");
+        privateKey = System.getenv("BT_PRIVATE_KEY");
+        if (merchantId == null || publicKey == null || privateKey == null){
+            throw new IllegalArgumentException("A Braintree Environment Variable is not correctly set. Check your .env file!");
         }
+        if(merchantId.equals("FILL_THIS") || publicKey.equals("FILL_THIS") || privateKey.equals("FILL_THIS")){
+            throw new IllegalArgumentException("A Braintree Environment Variable is still set to the default value 'FILL_THIS'." +
+                    "Replace it with a valid value.");
+        }
+
+        gateway = new BraintreeGateway(Environment.SANDBOX, merchantId, publicKey, privateKey);
+    }
+
+    public String generateClientToken(Integer userId){
+        ClientTokenRequest clientTokenRequest = new ClientTokenRequest().customerId(String.valueOf(userId));
+        return gateway.clientToken().generate(clientTokenRequest);
     }
 }
