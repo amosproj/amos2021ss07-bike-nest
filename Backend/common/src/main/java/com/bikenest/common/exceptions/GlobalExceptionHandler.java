b8113c6338180272199fa8f839bfad1c929e0b4d
Peter Herbst <60853627+richmandlx@users.noreply.github.com>
2021-06-03 12:26:45 +0200

    Also catch invalid request body errors in GlobalExceptionHandler #167

--- Backend/common/src/main/java/com/bikenest/common/exceptions/GlobalExceptionHandler.java
+++ Backend/common/src/main/java/com/bikenest/common/exceptions/GlobalExceptionHandler.java
@@ -4,13 +4,20 @@ import com.bikenest.common.interfaces.GeneralExceptionResponse;
 import com.bikenest.common.interfaces.GeneralResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.annotation.Priority;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -27,9 +34,7 @@ import javax.servlet.http.HttpServletRequest;
  * It is not that important to prevent this, but most other companies do it similar.
  */
 @ControllerAdvice
-public class GlobalExceptionHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
      * This ExceptionHandler catches all BusinessLogicExceptions and transforms then into a GeneralExceptionResponse.
@@ -41,8 +46,9 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(BusinessLogicException.class)
     public ResponseEntity<GeneralExceptionResponse> handleBusinessLogicExceptions(HttpServletRequest request, BusinessLogicException ex){
-        logger.info("BusinessLogicException:: URL=" + request.getRequestURL());
-        logger.info("Message:: " + ex.getMessage());
+        logger.error("BusinessLogicException:: URL=" + request.getRequestURL());
+        logger.error("Message:: " + ex.getMessage());
+        logger.error("StackTrace:: " + ex.getStackTrace());
         return ResponseEntity.unprocessableEntity().body(new GeneralExceptionResponse(ex.getMessage()));
     }
 
@@ -57,9 +63,26 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GeneralExceptionResponse> handleOtherExceptions(HttpServletRequest request, Exception ex){
-        logger.error("Unknown error occurred::" + ex.getMessage() + "\n Stacktrace::" + ex.getStackTrace());
+        logger.error("Unknown error::" + ex.getMessage());
+        logger.error("Stacktrace::" + ex.getStackTrace());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
                 new GeneralExceptionResponse("Ein unbekannter Fehler ist im Server aufgetreten."));
     }
 
+    /**
+     * This handles errors that will be generated, when an API Endpoint receives and invalid RequestBody.
+     * Hopefully this will prevent confusion on the frontend side (for example the signup endpoint would return
+     * a bad request response, when the password had less then 6 characters).
+     * @param ex
+     * @param headers
+     * @param status
+     * @param request
+     * @return
+     */
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+        logger.error("Invalid request::" + ex.getMessage());
+        return ResponseEntity.badRequest().body(new GeneralExceptionResponse("Fehlerhafte Anfrage!"));
+    }
 }
