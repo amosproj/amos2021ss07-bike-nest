673c13cdbba715c51f427b8739913872b9f86b64
Peter Herbst <60853627+richmandlx@users.noreply.github.com>
2021-06-09 16:54:24 +0200

    Update Kubernetes files.

--- Backend/dockerhub-push.bat
+++ Backend/dockerhub-push.bat
@@ -6,3 +6,16 @@ set /p pass=Password:
 call docker login -u %user% -p %pass%
 call docker build --tag bikenest/backend:service-bikenest ./service-bikenest/
 call docker push bikenest/backend:service-bikenest
+
+call docker build --tag bikenest/backend:service-booking ./service-booking/
+call docker push bikenest/backend:service-booking
+
+call docker build --tag bikenest/backend:service-payment ./service-payment/
+call docker push bikenest/backend:service-payment
+
+call docker build --tag bikenest/backend:service-usermgmt ./service-usermgmt/
+call docker push bikenest/backend:service-usermgmt
+
+call docker build --tag bikenest/backend:gateway ./apigateway/
+call docker push bikenest/backend:gateway
+
