b0fb794a4ce54341d211c5b45ffe575132cd99ad
Tobias Hassel <tobias.hassel1@web.de>
2021-06-21 20:45:29 +0200

    Add paymentservice tests

--- Backend/service-payment/src/test/java/com/bikenest/servicepayment/unit/PaymentServiceTest.java
+++ Backend/service-payment/src/test/java/com/bikenest/servicepayment/unit/PaymentServiceTest.java
@@ -24,7 +24,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= ServicepaymentApplication.class)
+@ContextConfiguration(classes = ServicepaymentApplication.class)
 public class PaymentServiceTest {
 
     @InjectMocks
@@ -33,57 +33,82 @@ public class PaymentServiceTest {
     @Mock
     PaymentRepository paymentRepository;
 
-
     @BeforeEach
     void setUpMocks() {
         Payment payment = new Payment(1, "DE01234567890123456789");
 
         MockitoAnnotations.openMocks(this);
-        when(paymentRepository.save(any())).thenAnswer(i -> i.getArguments()[0]);     
-        when(paymentRepository.findByUserId(1)).thenReturn(Optional.of(payment));        
-        when(paymentRepository.findByUserId(2)).thenReturn(Optional.empty());        
+        when(paymentRepository.save(any())).thenAnswer(i -> i.getArguments()[0]);
+        when(paymentRepository.findByUserId(1)).thenReturn(Optional.of(payment));
+        when(paymentRepository.findByUserId(2)).thenReturn(Optional.empty());
     }
-    
+
     @Test
-    void getPaymentByUserId_shouldReturnPayment_whenPaymentExists(){
-        //Arrange
+    void getPaymentByUserId_shouldReturnPayment_whenPaymentExists() {
+        // Arrange
         Integer userId = 1;
-        //Act
-        Optional<Payment> payment = paymentService.getPaymentByUserId(userId); 
-        //Assert
+        // Act
+        Optional<Payment> payment = paymentService.getPaymentByUserId(userId);
+        // Assert
         Assert.assertEquals(userId, payment.get().getUserId());
     }
 
     // @Test //(expected = NoSuchElementException.class)
     // void getPaymentByUserId_shouldThrow_whenPaymentDoesNotExist(){
-    //     //Arrange
-    //     Integer userId = 2;
-    //     //Act
-    //     Optional<Payment> payment = paymentService.getPaymentByUserId(userId); 
-    //     //Assert
-    //     //Assert.assertThrows(NoSuchElementException.class, () -> {payment.get();});
+    // //Arrange
+    // Integer userId = 2;
+    // //Act
+    // Optional<Payment> payment = paymentService.getPaymentByUserId(userId);
+    // //Assert
+    // //Assert.assertThrows(NoSuchElementException.class, () -> {payment.get();});
     // }
 
     @Test
-    void setIban_shouldSetIban() throws BusinessLogicException{
-        //Arrange
+    void setIban_shouldSetIban() throws BusinessLogicException {
+        // Arrange
         Integer userId = 1;
         String iban = "DE00000000000000000000";
-        //Act
+        // Act
         Payment payment = paymentService.setIban(userId, iban);
-        //Assert
+        // Assert
         Assert.assertEquals(iban, payment.getIban());
     }
 
     @Test
-    void setIban_shouldThrow_whenPaymentDoesNotExist() throws BusinessLogicException{
-        //Arrange
+    void setIban_shouldThrow_whenPaymentDoesNotExist() throws BusinessLogicException {
+        // Arrange
         Integer userId = 2;
         String iban = "DE00000000000000000000";
-        //Act
-        //Payment payment = paymentService.setIban(userId, iban);
-        //Assert
+
+        // Act
+        // Assert
         Assertions.assertThatThrownBy(() -> paymentService.setIban(userId, iban))
-        .isInstanceOf(BusinessLogicException.class);
+                .isInstanceOf(BusinessLogicException.class);
+    }
+
+    @Test
+    void createPayment_shouldCreateNewPaymentEntry_whenUserIdIsNew() throws BusinessLogicException {
+        // Arrange
+        Integer userId = 3;
+        String iban = "DE00000000000000000000";
+
+        // Act
+        boolean success = paymentService.createPayment(userId, iban);
+
+        // Assert
+        Assert.assertTrue(success);   
+    }
+
+    @Test
+    void createPayment_shouldFail_whenUserIdIsAlreadyUsed() throws BusinessLogicException {
+        // Arrange
+        Integer userId = 1;
+        String iban = "DE00000000000000000000";
+
+        // Act
+        boolean success = paymentService.createPayment(userId, iban);
+
+        // Assert
+        Assert.assertFalse(success);       
     }
 }
