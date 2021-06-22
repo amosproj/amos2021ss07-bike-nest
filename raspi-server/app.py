9eca95bcaffb093ec16b891dc33feb26130dc642
NikoBele1 <32575893+NikoBele1@users.noreply.github.com>
2021-06-22 17:30:14 +0200

    updated raspi functions

--- raspi-server/app.py
+++ raspi-server/app.py
@@ -6,12 +6,12 @@ import time
 
 app = Flask(__name__)
 # choose correct port e.g. not clear yet, can be /dev/ttyS0, /dev/serial1 or /dev/serial0 aswell
-# port = '/dev/ttyAMA0'
-ports = list(port_list.comports())
-print(ports)
+port = '/dev/serial0'
+#ports = list(port_list.comports())
+#print(ports)
 
 #port = '/dev/ttyS0'
-port = ports[0]
+#port = ports[0]
 baud = 57600
 
 # if needed add constants like this
@@ -22,7 +22,7 @@ CR = bytes.fromhex('0d')
 ACK_EOT = ACK+EOT
 
 # check timeout
-ser = serial.Serial(port, baud,timeout=1, parity=serial.PARITY_NONE)
+ser = serial.Serial(port, baud,timeout=6, parity=serial.PARITY_NONE)
 
 responseText = "no connection"
 
@@ -32,18 +32,18 @@ def hello():
     print("hello, world")
 
 @app.route('/returnPorts')
-def open_connection():
-    text = ""
+def returnPorts():
+    responseText = "available ports: "
     if ser.isOpen():
         ports = list(port_list.comports())
-        responseText = "available ports: " + ports
-    return text
+        for port in ports:
+            responseText+= port.name
+    return responseText
 
 @app.route('/openConnection')
 def open_connection():
     if ser.isOpen():
         responseText = ser.name + ' is open.'
-    responseText = ports
     return responseText
 
 @app.route('/')
@@ -61,36 +61,36 @@ def toggle_station_lock():
     else: 
         gate = "2"
     # Check status of station
-    ser.write('$11#0'+gate+'****<cr>'.encode('ascii'))
-    time.sleep(0.5)
+    ser.reset_input_buffer()
+    ser.write('$11#0'+gate+'****'.encode('ascii')+CR)
     #if station locked -> open
-    out = ser.read() 
-    if out ==  ACK + b'$11#0<cr>': 
-        ser.write('$01#0'+gate+'02**<cr>'.encode('ascii'))
-        time.sleep(0.5)
+    out = ser.read(7) 
+    if out ==  ACK + b'$11#0'+CR:
+        ser.reset_input_buffer()
+        ser.write('$01#0'+gate+'02**'.encode('ascii')+CR)
         # expecting ACK->EOT 
-        out = ser.read()  
+        out = ser.read(2)  
         if out == ACK_EOT:
             responseText = "Station Unlocked"
         else:
             responseText += " serial response = " + out.decode('ascii')
     # if station unlocked
-    elif out ==  ACK + b'$11#1<cr>': 
+    elif out ==  ACK + b'$11#1'+CR: 
         # ask gate state
-        ser.write(('$12#0'+gate+'****<cr>').encode('ascii'))
-        time.sleep(0.5)
-        out = ser.read() 
+        ser.reset_input_buffer()
+        ser.write(('$12#0'+gate+'****').encode('ascii')+CR)
+        out = ser.read(7) 
         #if gate open -> close gate
-        if out== ACK + b'$12#1<cr>':
-            ser.write(('$03#0'+gate+'****<cr>').encode('ascii'))
-            time.sleep(0.5)
-            out = ser.read() 
+        if out== ACK + b'$12#1'+CR:
+            ser.reset_input_buffer()
+            ser.write(('$03#0'+gate+'****').encode('ascii')+CR)
+            out = ser.read(2) 
             # lock station
             if out == ACK_EOT:
                 responseText = "gate closed, station lock unchanged"
-                ser.write('$01#0101**<cr>'.encode('ascii'))
-                time.sleep(0.5)
-                out = ser.read() 
+                ser.reset_input_buffer()
+                ser.write('$01#0101**'.encode('ascii')+CR)
+                out = ser.read(2) 
 
                 if out == ACK_EOT:
                    responseText = "gate closed, station locked"
@@ -119,24 +119,30 @@ def open_gate():
         return "Error, correct Gate was not detected!"
 
     #  Check if gate is already open
-    ser.write(('$12#0'+gate+'****<cr>').encode('ascii'))
-    time.sleep(0.5)
+    ser.reset_input_buffer()
+    ser.write(('$12#0'+gate+'****').encode('ascii')+CR)
 
     # check response here
-    out = ser.read() 
-    if out == ACK + b'$12#1<cr>':
-        ser.write(('$02#0'+gate+'****<cr>').encode('ascii'))
-        time.sleep(0.5)
-        out = ser.read() 
+    out = ser.read(7) 
+    if out == ACK + b'$12#1'+CR:
+        ser.reset_input_buffer()
+        ser.write(('$02#0'+gate+'****').encode('ascii')+CR)
+        out = ser.read(2) 
 
         if out == ACK_EOT:
             responseText = "gate opened"
         else:
-            responseText += " serial response = " + out.decode('ascii')
-    elif out == ACK + b'$12#0<cr>':
+            if out.decode('ascii') == "":
+                responseText += " TIMEOUT"
+            else:
+                responseText += " serial response = " + out.decode('ascii')
+    elif out == ACK + b'$12#0'+CR:
         responseText = "gate was already open"
     else:
-        responseText += " serial response = " + out.decode('ascii')
+        if out.decode('ascii') == "":
+            responseText += " TIMEOUT"
+        else:
+            responseText += " serial response = " + out.decode('ascii')
 
     return responseText
 
@@ -147,32 +153,38 @@ def close_gate():
     responseText = "error"
 
     if gate == 'left':
-        gate = 1
+        gate = '1'
     elif gate == 'right':
-        gate = 2
+        gate = '2'
     else:
         return "Error, correct Gate was not detected!"
 
     #Check if gate is already closed
-    ser.write(('$12#0'+gate+'****<cr>').encode('ascii'))
-    time.sleep(0.5)
+    ser.reset_input_buffer()
+    ser.write(('$12#0'+gate+'****').encode('ascii')+CR)
 
     #check response here
-    out = ser.read() 
-    if out == ACK + '$12#0<cr>':
+    out = ser.read(7) 
+    if out == ACK + b'$12#0'+CR:
         responseText += "gate already closed"
-    elif out == ACK + '$12#1<cr>':
-        ser.write(('$03#0'+gate+'****<cr>').encode('ascii'))
-        time.sleep(0.5)
-        out = ser.read() 
+    elif out == ACK + b'$12#1'+CR:
+        ser.reset_input_buffer()
+        ser.write(('$03#0'+gate+'****').encode('ascii')+CR)
+        out = ser.read(2) 
 
         if out == ACK_EOT:
             responseText = "gate opened"
         else:
-            responseText += " serial response = " + out.decode('ascii')
+            if out.decode('ascii') == "":
+                responseText += " TIMEOUT"
+            else:
+                responseText += " serial response = " + out.decode('ascii')
     else:
-        responseText += " serial response = " + out.decode('ascii')
-    return '''$03#0{}****<cr>'''.format(gate)
+        if out.decode('ascii') == "":
+            responseText += " TIMEOUT"
+        else:
+            responseText += " serial response = " + out.decode('ascii')
+    return responseText
 
 
 # @app.route('/show_booked_spot')
@@ -184,7 +196,7 @@ def close_gate():
 #     #nn = number of spot [1 - 20]
 #     #rgb = color of LED [0 == off, 1 == on]
 #     #b = LED state [0 == off, 1 == blink]
-#     return '''$04#{}{}{}<cr>'''.format(spot_number, rgb_value, blink_state)
+#     return '''$04#{}{}{}+CR'''.format(spot_number, rgb_value, blink_state)
 
 @app.route('/set_spot_reserved')
 def set_spot_reserved():
@@ -196,13 +208,16 @@ def set_spot_reserved():
 
     spot_number = str(spot_number).zfill(2)
     
-    ser.write(('$04#'+spot_number+color+blink_state+'<cr>').encode('ascii'))
-    time.sleep(0.5)
-    out = ser.read()
+    ser.reset_input_buffer()
+    ser.write(('$04#'+spot_number+color+blink_state+'').encode('ascii')+CR)
+    out = ser.read(2)
     if out == ACK_EOT:
             responseText = "gate opened"
     else:
-        responseText += " serial response = " + out.decode('ascii')
+        if out.decode('ascii') == "":
+            responseText += " TIMEOUT"
+        else:
+            responseText += " serial response = " + out.decode('ascii')
 
     return responseText
 
@@ -211,17 +226,19 @@ def get_status_station_lock():
     responseText = 'error'
 
     # Check if locked or unlocked
-    ser.write('$11#01****<cr>'.encode('ascii'))
-    time.sleep(0.5)
-
+    ser.reset_input_buffer()
+    ser.write('$11#01****'.encode('ascii')+CR)
     # check response here
-    out = ser.read() 
-    if out=='$11#1<cr>':
+    out = ser.read(7)
+    if out==ACK+b'$11#1'+CR:
         responseText = 'open'
-    elif out=='$11#0<cr>':
+    elif out==ACK+b'$11#0'+CR:
         responseText = 'closed'
     else:
-        responseText += " serial response = " + out.decode('ascii')
+        if out.decode('ascii') == "":
+            responseText += " TIMEOUT"
+        else:
+            responseText += " serial response = " + out.decode('ascii')
     return responseText
 
 @app.route('/get_status_gate_position')
@@ -231,23 +248,26 @@ def get_status_gate_position():
     gate = request.args.get('gate')
 
     if gate == 'left':
-        gate = 1
+        gate = '1'
     elif gate == 'right':
-        gate = 2
+        gate = '2'
     else:
         return "Error, correct Gate was not detected!"
 
     # Check if open or closed
-    ser.write(('$12#'+gate+'****<cr>').encode('ascii'))
-    time.sleep(1)
-
-    out = ser.read()
-    if out=='$12#1<cr>':
+    ser.reset_input_buffer()
+    ser.write(('$12#'+gate+'****').encode('ascii')+CR)
+    
+    out = ser.read(7)
+    if out==ACK+b'$12#1'+CR:
          responseText = 'open'
-    elif out=='$12#0<cr>':
+    elif out==ACK+b'$12#0'+CR:
          responseText = 'closed'
     else:
-        responseText += " serial response = " + out.decode('ascii')    
+        if out.decode('ascii') == "":
+            responseText += " TIMEOUT"
+        else:
+            responseText += " serial response = " + out.decode('ascii')
 
     return responseText
 
@@ -259,15 +279,19 @@ def get_status_bikespot():
     spot = str(spot).zfill(2)
 
     # Check if occupied or empty
-    ser.write(('$13#'+spot+'****<cr>').encode('ascii'))
-    time.sleep(0.5)    
-    out = ser.read()  
-    if out=='$13#0<cr>':
+    ser.reset_input_buffer()
+    ser.write(('$13#'+spot+'****').encode('ascii')+CR)
+
+    out = ser.read(7)  
+    if out==ACK + b'$13#0'+CR:
         responseText = 'empty'
-    elif out =='$13#1<cr>':
+    elif out == ACK + b'$13#1'+CR:
         responseText = 'occupied'
     else:
-        responseText += " serial response = " + out.decode('ascii') 
+        if out.decode('ascii') == "":
+            responseText += " TIMEOUT"
+        else:
+            responseText += " serial response = " + out.decode('ascii')
     return responseText
 
 @app.route('/get_error_status')
